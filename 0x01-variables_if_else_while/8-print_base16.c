5fb8dd7b4baf1ffc11bc27f2775dccd0a1885ee7
lumicode <pelumose@gmail.com>
2025-11-11 23:38:37 +0100

    add program that prints whether number is positive, negative, or zero

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 16; n++)
+	{
+		if (n < 10)
+			putchar(n + '0');
+		else
+			putchar(n - 10 + 'a');
+	}
+	putchar('\n');
+	return (0);
+}
